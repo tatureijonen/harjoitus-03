fa23083db69a988b92ccf82f85e5b6e575cabd81
tatureijonen <reisca.reijonmaa@gmail.com>
2014-09-24 11:05:48 +0300

    Harjoitus 3 - IIO14S2 - Tatu Reijonen

---
+++ harj3a.cpp
@@ -0,0 +1,39 @@
+/*/
+TATU REIJONEN - IIO14S2
+
+Harjoitus 3 (palautus vko 39)
+
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout << "Hei maailma!";
+}
+
+Muokkaa ylla olevaa ohjelmaa niin, etta tulostat oman nimesi
+ja osoitteesi naytolle allekkain.
+HUOM!Nimeä tiedosto harj3a.cpp nimelle!
+
+Muokkaa edellisestä ohjelmasta versio nimelle harj3b.cpp, jossa
+nimi ja osoite tulostetaan näytälle seuraavasti :
+
+**********************************
+* *
+* Etunimi *
+* Sukunimi *
+* *
+*-------------------------------------- *
+* *
+* Asemakatu 7 *
+* 40520 JKL *
+* *
+**********************************
+__________________________________________________________________________
+/*/
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout << "Tatu Reijonen\n"
+		<< "Kortesuonkatu 3 B 14";
+}
---
+++ harj3b.cpp
@@ -0,0 +1,55 @@
+/*
+TATU REIJONEN - IIO14S2
+
+Harjoitus 3 (palautus vko 39)
+
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout << "Hei maailma!";
+}
+
+Muokkaa ylla olevaa ohjelmaa niin, etta tulostat oman nimesi
+ja osoitteesi naytolle allekkain.
+HUOM!Nime‰ tiedosto harj3a.cpp nimelle!
+
+Muokkaa edellisest‰ ohjelmasta versio nimelle harj3b.cpp, jossa
+nimi ja osoite tulostetaan n‰yt‰lle seuraavasti :
+
+**********************************
+* *
+* Etunimi *
+* Sukunimi *
+* *
+*-------------------------------------- *
+* *
+* Asemakatu 7 *
+* 40520 JKL *
+* *
+**********************************
+__________________________________________________________________________
+/*
+
+
+/* Kommentti alkaa
+ja jatkuu
+ja jatkuu ...*/
+#include <iostream> // otsikkotiedosto sis‰llytet‰‰n ohjelmaan
+using namespace std;
+void main(void) // p‰‰ohjelma alkaa
+{
+	cout << "**********************************\n"
+		<< "* *\n"
+		<< "* Tatu\n"
+		<< "* Reijonen\n"
+		<< "* *\n"
+		<< "*-------------------------------------- *\n"
+		<< "* *\n"
+		<< "* Kortesuonkatu 3 B 14\n"
+		<< "* 40700 JKL\n"
+		<< "* *\n"
+		<< "**********************************";
+
+
+}
